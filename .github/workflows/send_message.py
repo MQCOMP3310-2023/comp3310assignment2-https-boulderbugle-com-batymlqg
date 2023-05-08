b34f01a8d8fef9466c6582be299e6d941856de43
MyMelodyUwU <64640893+MyMelodyUwU@users.noreply.github.com>
2023-05-08 10:43:55 +1000

    add discord integration

---
+++ .github/workflows/send_message.py
@@ -0,0 +1,23 @@
+import requests
+
+# Read the webhook ID and token from id.txt and token.txt respectively
+with open("id.txt", "r") as id_file, open("token.txt", "r") as token_file:
+    webhook_id = id_file.read().strip()
+    webhook_token = token_file.read().strip()
+
+# Define the message to send
+message = "A new push has been made to the repository!"
+
+# Define the payload to send to the webhook
+payload = {
+    "content": message
+}
+
+# Construct the webhook URL
+webhook_url = f"https://discord.com/api/webhooks/{webhook_id}/{webhook_token}"
+
+# Send the payload to the webhook URL
+response = requests.post(webhook_url, json=payload)
+
+# Print the status code of the response
+print(response.status_code)
