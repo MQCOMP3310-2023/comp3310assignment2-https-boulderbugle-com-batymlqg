0722d6b46df86b70e5fd866fd4f35c5bc9333309
MyMelodyUwU <64640893+MyMelodyUwU@users.noreply.github.com>
2023-05-22 18:54:12 +1000

    SEARCH BAR IS UP!

--- project/main.py
+++ project/main.py
@@ -7,15 +7,24 @@ import requests
 import logging
 import os
 import wave
+from google.cloud import speech_v1p1beta1
+from google.cloud import speech_v1p1beta1 as speech
+from google.cloud.speech_v1p1beta1 import enums
+from google.cloud.speech_v1p1beta1 import types
+from google.oauth2 import service_account
+from nltk.corpus import wordnet
+from sqlalchemy import or_
+
 
 main = Blueprint('main', __name__)
-
 
 @main.route('/')
 @main.route('/restaurant/')
 def showRestaurants():
-  restaurants = db.session.query(Restaurant).order_by(asc(Restaurant.name))
-  return render_template('restaurants.html', restaurants = restaurants)
+    restaurants = db.session.query(Restaurant).order_by(asc(Restaurant.name))
+    show_search_bar = True  # Set the value based on your logic
+
+    return render_template('restaurants.html', restaurants=restaurants, show_search_bar=show_search_bar)
 
 #Create a new restaurant
 @main.route('/restaurant/new/', methods=['GET','POST'])
@@ -54,7 +63,6 @@ def deleteRestaurant(restaurant_id):
   else:
     return render_template('deleteRestaurant.html',restaurant = restaurantToDelete)
 
-#Show a restaurant menu
 # @main.route('/restaurant/<int:restaurant_id>/')
 # @main.route('/restaurant/<int:restaurant_id>/menu/')
 # def showMenu(restaurant_id):
@@ -171,20 +179,131 @@ def search_menu_items(restaurant_id):
     else:
         return redirect(url_for('main.showMenu', restaurant_id=restaurant_id))
 
+# @main.route('/search/', methods=['GET', 'POST'])
+# def search_restaurant():
+#     query = request.args.get('q', '')
+#     print(query)
+#     if query:
+#         word_list = create_word_list(query)
+#         print("Word List:", word_list)
+
+#         conditions = [Restaurant.name.ilike(f'%{word}%') for word in word_list]
+#         conditions += [MenuItem.name.ilike(f'%{word}%') for word in word_list]
+#         conditions += [MenuItem.description.ilike(f'%{word}%') for word in word_list]
+#         query = db.session.query(Restaurant).filter(or_(*conditions))
+#         print("Query:", conditions)
+
+#         restaurant = query.all()
+#         print(restaurant)
+#         restaurant = query.all()
+#         print(restaurant)
+#         if restaurant:
+#             restaurant_id = restaurant.id
+#             print("Restaurant ID:", restaurant_id)
+#             items = db.session.query(MenuItem).filter(MenuItem.restaurant_id == restaurant_id).all()
+#             print("Items:", items)
+#             return redirect(url_for('main.showMenu', restaurant_id=restaurant_id))
+#     flash('Please enter a valid search query')
+#     return redirect(url_for('main.showRestaurants'))
+
+from collections import Counter
+
 @main.route('/search/', methods=['GET', 'POST'])
 def search_restaurant():
     query = request.args.get('q', '')
-    print("Query:", query)  # Print the query for debugging purposes
+    print(query)
     if query:
-        restaurant = db.session.query(Restaurant).filter(Restaurant.name.ilike(f'%{query}%')).first()
-        if restaurant:
-            restaurant_id = restaurant.id
-            print(restaurant_id)
-            items = db.session.query(MenuItem).filter(MenuItem.name.ilike(f'%{restaurant_id}%')).all()
-            print(items)
+        word_list = create_word_list(query)
+        print("Word List:", word_list)
+
+        conditions = [Restaurant.name.ilike(f'%{word}%') for word in word_list]
+        conditions += [MenuItem.name.ilike(f'%{word}%') for word in word_list]
+        conditions += [MenuItem.description.ilike(f'%{word}%') for word in word_list]
+        query = db.session.query(Restaurant).filter(or_(*conditions))
+        print(query.params)
+        print("Query:", conditions)
+
+        rows = query.all()
+        print("Rows:")
+        for row in rows:
+            print(row)
+
+        restaurant_counter = Counter(row.name for row in rows)
+        most_common = restaurant_counter.most_common()
+        print("Most Common Occurrences:")
+        for name, count in most_common:
+            print(f"{name}: {count}")
+
+        word_counter = Counter(word for row in rows for word in word_list)
+        word_counts = word_counter.items()
+        print("Word Counts:")
+        for word, count in word_counts:
+            print(f"{word}: {count}")
+
+        if rows:
+            restaurant_id = rows[0].id
+            print("Restaurant ID:", restaurant_id)
+            items = db.session.query(MenuItem).filter(MenuItem.restaurant_id == restaurant_id).all()
+            print("Items:", items)
             return redirect(url_for('main.showMenu', restaurant_id=restaurant_id))
+    
     flash('Please enter a valid search query')
     return redirect(url_for('main.showRestaurants'))
+
+
+# @main.route('/search/', methods=['GET', 'POST'])
+# def search_restaurant():
+#     query = request.args.get('q', '')
+#     print(query)
+#     if query:
+#         word_list = create_word_list(query)
+#         print("Word List:", word_list)
+
+#         conditions = [Restaurant.name.ilike(f'%{word}%') for word in word_list]
+#         conditions += [MenuItem.name.ilike(f'%{word}%') for word in word_list]
+#         conditions += [MenuItem.description.ilike(f'%{word}%') for word in word_list]
+#         query = db.session.query(Restaurant).filter(or_(*conditions))
+#         print(query.params)
+#         print("Query:", conditions)
+
+#         rows = query.all()
+#         print("Rows:")
+#         for row in rows:
+#             print(row)
+
+#         restaurant_counter = Counter(row.name for row in rows)
+#         most_common = restaurant_counter.most_common()
+#         print("Most Common Occurrences:")
+#         for name, count in most_common:
+#             print(f"{name}: {count}")
+
+#         if rows:
+#             restaurant_id = rows[0].id
+#             print("Restaurant ID:", restaurant_id)
+#             items = db.session.query(MenuItem).filter(MenuItem.restaurant_id == restaurant_id).all()
+#             print("Items:", items)
+#             return redirect(url_for('main.showMenu', restaurant_id=restaurant_id))
+    
+#     flash('Please enter a valid search query')
+#     return redirect(url_for('main.showRestaurants'))
+
+
+def create_word_list(query):
+    word_list = query.split()
+
+    for i in range(len(word_list)):
+        word = word_list[i].lower()
+
+        synsets = wordnet.synsets(word)
+
+        for synset in synsets:
+            for synonym in synset.lemmas():
+                similar_word = synonym.name().replace("_", " ")
+                word_list.append(similar_word)
+
+    word_list = list(set(word_list))
+    return word_list
+
 
 
 # import wave
@@ -264,24 +383,16 @@ def search_restaurant():
 
 #     return transcribed_text
 
-import os
-import wave
-from google.cloud import speech_v1p1beta1 as speech
-from google.cloud.speech_v1p1beta1 import enums
-from google.cloud.speech_v1p1beta1 import types
-from google.oauth2 import service_account
-
-# Set the path to your JSON credentials file
 credentials_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'google_search_keys', 'food-api-386807-89a88d306a8d.json')
 
-# Create a credentials object from the JSON file
 credentials = service_account.Credentials.from_service_account_file(credentials_path)
 
 os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = credentials_path
 
 @main.route("/speech", methods=["GET", "POST"])
 def speech():
-    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'voicetest', 'UrbanBurgerAI.wav')
+    #file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'voicetest', 'PandaGarden.wav')
+    file_path = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'voicetest', 'AAD.wav')
     audio_data, sample_width, num_channels, frame_rate = read_wav_file(file_path)
 
     print("Sample width:", sample_width)
@@ -289,10 +400,22 @@ def speech():
     print("Frame rate:", frame_rate)
 
     transcribed_text = transcribe_audio(audio_data, sample_width, frame_rate)
+    word_list = transcribed_text.split()
 
-    print("Transcribed text:", transcribed_text)
+    if transcribed_text:
+        conditions = [Restaurant.name.ilike(f'%{word}%') for word in word_list]
+        query = db.session.query(Restaurant).filter(or_(*conditions))
+        
+        restaurant = query.first()
+        if restaurant:
+            restaurant_id = restaurant.id
+            print("Restaurant ID:", restaurant_id)
+            items = db.session.query(MenuItem).filter(MenuItem.restaurant_id == restaurant_id).all()
+            print("Items:", items)
+            return redirect(url_for('main.showMenu', restaurant_id=restaurant_id))
 
-    return "Speech processing completed"
+    flash('Please enter a valid search query')
+    return redirect(url_for('main.showRestaurants'))
 
 def read_wav_file(file_path):
     with wave.open(file_path, 'rb') as wav_file:
@@ -305,7 +428,42 @@ def read_wav_file(file_path):
 
     return audio_data, sample_width, num_channels, frame_rate
 
-from google.cloud import speech_v1p1beta1
+
+def transcribe_audio(audio_data, sample_width, frame_rate):
+    client = speech_v1p1beta1.SpeechClient(credentials=credentials)
+    print(credentials)
+
+    config = {
+        "encoding": enums.RecognitionConfig.AudioEncoding.LINEAR16,
+        "sample_rate_hertz": frame_rate,
+        "language_code": "en-US",
+    }
+
+    audio = {"content": audio_data}
+
+    response = client.recognize(config=config, audio=audio)
+
+    print("Response:", response) 
+
+    transcribed_text = ""
+    for result in response.results:
+        if result.alternatives:
+            transcribed_text += result.alternatives[0].transcript
+
+    print("Transcribed text:", transcribed_text)
+
+    return transcribed_text
+
+def read_wav_file(file_path):
+    with wave.open(file_path, 'rb') as wav_file:
+        sample_width = wav_file.getsampwidth()
+        num_channels = wav_file.getnchannels()
+        frame_rate = wav_file.getframerate()
+        num_frames = wav_file.getnframes()
+
+        audio_data = wav_file.readframes(num_frames)
+
+    return audio_data, sample_width, num_channels, frame_rate
 
 def transcribe_audio(audio_data, sample_width, frame_rate):
     client = speech_v1p1beta1.SpeechClient(credentials=credentials)
@@ -320,8 +478,6 @@ def transcribe_audio(audio_data, sample_width, frame_rate):
 
     response = client.recognize(config=config, audio=audio)
 
-    print("Response:", response)  # Debug print for response object
-
     transcribed_text = ""
     for result in response.results:
         if result.alternatives:
@@ -332,32 +488,19 @@ def transcribe_audio(audio_data, sample_width, frame_rate):
     return transcribed_text
 
 
-# def index():
-#     transcript = ""
-#     if request.method == "POST":
-#         print("FORM DATA RECEIVED")
 
-#         if "file" not in request.files:
-#             return redirect(request.url)
+def create_word_list(transcribed_text):
+    word_list = transcribed_text.split()
 
-#         file = request.files["file"]
-#         if file.filename == "":
-#             return redirect(request.url)
+    for i in range(len(word_list)):
+        word = word_list[i].lower()
 
-#         if file:
-#             recognizer = sr.Recognizer()
-#             audioFile = sr.AudioFile(file)
-#             with audioFile as source:
-#                 data = recognizer.record(source)
-#             transcript = recognizer.recognize_google(data, key=None)
+        synsets = wordnet.synsets(word)
 
-#         if query:
-#             restaurant = db.session.query(Restaurant).filter(Restaurant.name.ilike(f'%{query}%')).first()
-#             if restaurant:
-#                 restaurant_id = restaurant.id
-#                 print(restaurant_id)
-#                 items = db.session.query(MenuItem).filter(MenuItem.name.ilike(f'%{restaurant_id}%')).all()
-#                 print(items)
-#                 return redirect(url_for('main.showMenu', restaurant_id=restaurant_id))
-#     flash('Bro you fucked up')
-#     return render_template('main.showRestraunts', transcript=transcript)
+        for synset in synsets:
+            for synonym in synset.lemmas():
+                similar_word = synonym.name().replace("_", " ")
+                word_list.append(similar_word)
+
+    word_list = list(set(word_list))
+    return word_list
