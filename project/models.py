efd85bca981f83d975299fe7a360b826141c4c8a
Rogueware <marcus.tl.lee@hotmail.com>
2023-05-23 23:11:47 +1000

    Corrected User Models

--- project/models.py
+++ project/models.py
@@ -1,3 +1,4 @@
+from flask_login import UserMixin
 from . import db
 
 class Restaurant(db.Model):
@@ -32,7 +33,13 @@ class MenuItem(db.Model):
            'course'     : self.course,
        }
 
-class Users(db.Model):
-    id = db.Column(db.Interger, primary_key = True)
-    email = db.Column(db.String(80), nullable = False)
-    password = db.Column(db.String(70), nullable = False)
+class User(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True) # primary keys are required by SQLAlchemy
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+    name = db.Column(db.String(1000))
+
+# class Users(db.Model):
+#     id = db.Column(db.Interger, primary_key = True)
+#     email = db.Column(db.String(80), nullable = False)
+#     password = db.Column(db.String(70), nullable = False)
