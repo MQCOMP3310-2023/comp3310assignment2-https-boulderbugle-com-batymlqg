4dd6e08b0d8b15f0e0bae19b699b935b3599dcf7
Rogueware <marcus.tl.lee@hotmail.com>
2023-05-24 18:33:59 +1000

    Implemented working signup,sign in and sign out

--- project/auth.py
+++ project/auth.py
@@ -1,4 +1,5 @@
 from flask import Blueprint, render_template, redirect, url_for, request, flash
+from flask_login import login_user, login_required, logout_user
 from werkzeug.security import generate_password_hash, check_password_hash
 from .models import User
 from . import db
@@ -8,6 +9,25 @@ auth = Blueprint('auth', __name__)
 @auth.route('/login')
 def login():
     return render_template('login.html')
+
+@auth.route('/login', methods=['POST'])
+def login_post():
+    email = request.form.get('email')
+    password = request.form.get('password')
+    remember = True if request.form.get('remember') else False
+
+    user = User.query.filter_by(email=email).first()
+
+    # check if the user actually exists
+    # take the user-supplied password and compare it with the stored password
+    if not user or not check_password_hash(user.password,password):
+        flash('Please check your login details and try again.')
+        current_app.logger.warning("User login failed")
+        return redirect(url_for('auth.login')) # if the user doesn't exist or password is wrong, reload the page
+
+    # if the above check passes, then we know the user has the right credentials
+    login_user(user, remember=remember)
+    return redirect("/")
 
 @auth.route('/signup')
 def signup():
@@ -36,5 +56,7 @@ def signup_post():
     return redirect(url_for('auth.login'))
 
 @auth.route('/logout')
+@login_required
 def logout():
-    return 'Logout'
+    logout_user();
+    return redirect("/")
